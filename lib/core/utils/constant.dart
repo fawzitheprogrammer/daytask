4ab8a69d7a05dc0cbdcd155d652ada51a1f36c1d
Fawzi Gharib <fawz11gharib@gmail.com>
2024-01-27 19:35:30 +0300

    init commit

---
+++ lib/core/utils/constant.dart
@@ -0,0 +1,32 @@
+class AppConstant {
+  // SVG Icons
+  static const String ICON_ADD = "assets/svg/icon/add.svg";
+  static const String ICON_APP_LOGO = "assets/svg/icon/app_logo.svg";
+  static const String ICON_ARROW_DOWN_2 = "assets/svg/icon/arrowdown2.svg";
+  static const String ICON_ARROW_LEFT = "assets/svg/icon/arrowleft.svg";
+  static const String ICON_CALENDAR = "assets/svg/icon/calendar.svg";
+  static const String ICON_CALENDAR_REMOVE =
+      "assets/svg/icon/calendarremove.svg";
+  static const String ICON_CLOCK = "assets/svg/icon/clock.svg";
+  static const String ICON_CLOSE_SQUARE = "assets/svg/icon/closesquare.svg";
+  static const String ICON_EDIT = "assets/svg/icon/edit.svg";
+  static const String ICON_ELEMENT_EQUAL = "assets/svg/icon/elementequal.svg";
+  static const String ICON_EYELASH = "assets/svg/icon/eyelash.svg";
+  static const String ICON_HOME_2 = "assets/svg/icon/home2.svg";
+  static const String ICON_LOCK_1 = "assets/svg/icon/lock1.svg";
+  static const String ICON_MESSAGES_1 = "assets/svg/icon/messages1.svg";
+  static const String ICON_MICROPHONE_2 = "assets/svg/icon/microphone2.svg";
+  static const String ICON_NOTIFICATION_1 = "assets/svg/icon/notification1.svg";
+  static const String ICON_PROFILE_USER = "assets/svg/icon/profile2user.svg";
+  static const String ICON_SEARCH_NORMAL_1 =
+      "assets/svg/icon/searchnormal1.svg";
+  static const String ICON_SECURITY_CARD = "assets/svg/icon/securitycard.svg";
+  static const String ICON_SEND = "assets/svg/icon/send.svg";
+  static const String ICON_SETTINGS_2 = "assets/svg/icon/setting2.svg";
+  static const String ICON_SETTINGS_4 = "assets/svg/icon/setting4.svg";
+  static const String ICON_TASK = "assets/svg/icon/task.svg";
+  static const String ICON_USER_ADD = "assets/svg/icon/useradd.svg";
+  static const String ICON_USER_TAG = "assets/svg/icon/usertag.svg";
+
+  // ... include additional constants as needed for the rest of the files
+}
