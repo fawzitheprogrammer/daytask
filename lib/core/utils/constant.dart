1f3002fdf09b80f734287981c2d1bbe9348d0468
Fawzi Gharib <fawz11gharib@gmail.com>
2024-01-27 19:49:34 +0300

    Icons Added & Pubspec Edited

--- lib/core/utils/constant.dart
+++ lib/core/utils/constant.dart
@@ -1,32 +1,30 @@
 class AppConstant {
   // SVG Icons
-  static const String ICON_ADD = "assets/svg/icon/add.svg";
-  static const String ICON_APP_LOGO = "assets/svg/icon/app_logo.svg";
-  static const String ICON_ARROW_DOWN_2 = "assets/svg/icon/arrowdown2.svg";
-  static const String ICON_ARROW_LEFT = "assets/svg/icon/arrowleft.svg";
-  static const String ICON_CALENDAR = "assets/svg/icon/calendar.svg";
-  static const String ICON_CALENDAR_REMOVE =
-      "assets/svg/icon/calendarremove.svg";
-  static const String ICON_CLOCK = "assets/svg/icon/clock.svg";
-  static const String ICON_CLOSE_SQUARE = "assets/svg/icon/closesquare.svg";
-  static const String ICON_EDIT = "assets/svg/icon/edit.svg";
-  static const String ICON_ELEMENT_EQUAL = "assets/svg/icon/elementequal.svg";
-  static const String ICON_EYELASH = "assets/svg/icon/eyelash.svg";
-  static const String ICON_HOME_2 = "assets/svg/icon/home2.svg";
-  static const String ICON_LOCK_1 = "assets/svg/icon/lock1.svg";
-  static const String ICON_MESSAGES_1 = "assets/svg/icon/messages1.svg";
-  static const String ICON_MICROPHONE_2 = "assets/svg/icon/microphone2.svg";
-  static const String ICON_NOTIFICATION_1 = "assets/svg/icon/notification1.svg";
-  static const String ICON_PROFILE_USER = "assets/svg/icon/profile2user.svg";
-  static const String ICON_SEARCH_NORMAL_1 =
-      "assets/svg/icon/searchnormal1.svg";
-  static const String ICON_SECURITY_CARD = "assets/svg/icon/securitycard.svg";
-  static const String ICON_SEND = "assets/svg/icon/send.svg";
-  static const String ICON_SETTINGS_2 = "assets/svg/icon/setting2.svg";
-  static const String ICON_SETTINGS_4 = "assets/svg/icon/setting4.svg";
-  static const String ICON_TASK = "assets/svg/icon/task.svg";
-  static const String ICON_USER_ADD = "assets/svg/icon/useradd.svg";
-  static const String ICON_USER_TAG = "assets/svg/icon/usertag.svg";
+  static const String iconAdd = "assets/svg/icon/add.svg";
+  static const String iconAppLogo = "assets/svg/icon/app_logo.svg";
+  static const String iconArrowDown = "assets/svg/icon/arrowdown.svg";
+  static const String iconArrowLeft = "assets/svg/icon/arrowleft.svg";
+  static const String iconCalendar = "assets/svg/icon/calendar.svg";
+  static const String iconCalendarRemove = "assets/svg/icon/calendarremove.svg";
+  static const String iconClock = "assets/svg/icon/clock.svg";
+  static const String iconCloseSquare = "assets/svg/icon/closesquare.svg";
+  static const String iconEdit = "assets/svg/icon/edit.svg";
+  static const String iconElementEqual = "assets/svg/icon/elementequal.svg";
+  static const String iconEyelash = "assets/svg/icon/eyelash.svg";
+  static const String iconHome = "assets/svg/icon/home.svg";
+  static const String iconLock = "assets/svg/icon/lock.svg";
+  static const String iconMessages = "assets/svg/icon/messages.svg";
+  static const String iconMicrophone = "assets/svg/icon/microphone.svg";
+  static const String iconNotification = "assets/svg/icon/notification.svg";
+  static const String iconProfileUser = "assets/svg/icon/profileuser.svg";
+  static const String iconSearchNormal = "assets/svg/icon/searchnormal.svg";
+  static const String iconSecurityCard = "assets/svg/icon/securitycard.svg";
+  static const String iconSend = "assets/svg/icon/send.svg";
+  static const String iconSetting = "assets/svg/icon/setting.svg";
+  static const String iconFilter = "assets/svg/icon/filter.svg";
+  static const String iconTask = "assets/svg/icon/task.svg";
+  static const String iconUserAdd = "assets/svg/icon/useradd.svg";
+  static const String iconUserTag = "assets/svg/icon/usertag.svg";
 
   // ... include additional constants as needed for the rest of the files
 }
